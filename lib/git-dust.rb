10ac03aaef17c52a3195fc73e853f52a1a599e2c
Yuya.Nishida <yuya@j96.org>
2014-07-04 01:45:09 +0900

    support "git dust squash".

--- lib/git-dust.rb
+++ lib/git-dust.rb
@@ -48,6 +48,37 @@ EOS
     run_command(*%w(git commit -m), COMMIT_MESSAGE, *args)
   end
 
+  def self.squash(args)
+    base_sha1 = find_non_dust_commit
+    saved_editor_environment = ENV["GIT_SEQUENCE_EDITOR"]
+    begin
+      ENV["GIT_SEQUENCE_EDITOR"] = "git dust edit-rebase-commit-list"
+      run_command(*%w(git rebase -i), base_sha1)
+    ensure
+      ENV["GIT_SEQUENCE_EDITOR"] = saved_editor_environment
+    end
+    run_command(*%w(git reset --soft HEAD^))
+    run_command("git commit --edit")
+  end
+
+  def self.edit_rebase_commit_list(args)
+    output = []
+    rebase_todo_path = Pathname(args.first)
+    rebase_todo_path.open do |f|
+      lines = f.each_line
+      lines.each do |l| # write first commit
+        if !/\A\s*#/.match(l)
+          output << l
+          break
+        end
+      end
+      lines.each do |l| # change "pick" to "fixup"
+        output << l.sub(/\Apick/, "fixup")
+      end
+    end
+    rebase_todo_path.write(output.join)
+  end
+
   private
 
   COMMIT_MESSAGE = "git-dust commit.".freeze
@@ -56,6 +87,16 @@ EOS
     if !system(*args)
       raise "failed: args=<#{args.inspect}>"
     end
+  end
+
+  def self.find_non_dust_commit
+    Open3.popen2("git log --format=oneline") do |stdin, stdout, wait_thread|
+      stdout.each_line.each do |line|
+        sha1, subject = line.chomp.split(" ", 2)
+        return sha1 if COMMIT_MESSAGE != subject
+      end
+    end
+    raise "non dust commit is not exist."
   end
 end
 
