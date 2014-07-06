f4335d1062355319af40875a46624a83f8d34357
Yuya.Nishida <yuya@j96.org>
2014-07-07 00:59:21 +0900

    move creating repository code.

--- spec/git/dust_spec.rb
+++ spec/git/dust_spec.rb
@@ -1,31 +1,38 @@
 require "spec_helper"
 
 RSpec.describe Git::Dust do
+  let(:g) {
+    Git.init(Dir.mktmpdir)
+  }
+
+  let(:file1_path) {
+    Pathname(g.dir.path) + "a.txt"
+  }
+
+  before(:each) do
+    g.config("user.name", "git-dust test user")
+    g.config("user.email", "anonymous@git-dust.example.org")
+    file1_path.write("1\n")
+    g.add(all: true)
+    g.commit("first import.")
+  end
+
+  after(:each) do
+    Pathname(g.dir.path).rmtree
+  end
+
   describe ".commit" do
     describe "empty args" do
       it "commit staging files with commit message" do
-        Dir.mktmpdir do |d|
-          tmp_path = Pathname(d)
+        file1_path.write("2\n")
+        g.add(all: true)
 
-          g = Git.init(tmp_path.to_s)
-          g.config("user.name", "git-dust test user")
-          g.config("user.email", "anonymous@git-dust.example.org")
-
-          file1_path = tmp_path + "a.txt"
-          file1_path.write("1\n")
-          g.add(all: true)
-          g.commit("first import.")
-
-          file1_path.write("2\n")
-          g.add(all: true)
-
-          g.chdir do
-            expect do
-              Git::Dust.commit([])
-            end.to change {g.log.count}.by(1)
-          end
-          expect(g.log.first.message).to eq("git dust commit.")
+        g.chdir do
+          expect do
+            Git::Dust.commit([])
+          end.to change {g.log.count}.by(1)
         end
+        expect(g.log.first.message).to eq("git dust commit.")
       end
     end
   end
